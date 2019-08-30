19c3a1cd47eade18f2ef1227befaf84bbd88bbbf
Bruno Valle <brunolv6@gmail.com>
2019-08-30 11:32:03 -0300

    Pass state and JobItem component to App component to deploy on github

--- src/components/search-bar/search-bar.components.jsx
+++ src/components/search-bar/search-bar.components.jsx
@@ -1,64 +1,15 @@
 import React from 'react';
 
-import JobItem from '../job-item/job-item.component';
-
 import './search-bar.style.scss'
 
-class SearchBar extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            search: '',
-            vagas: null
-        }
-    }
-
-    handleChange = event => {
-        this.setState({search: event.target.value});
-    }
-
-    handleSubmit = event => {
-        //previne o browser de reload/refresh!!!
-        event.preventDefault();
-        fetch('https://api-search-jobs.herokuapp.com/', {
-            method: 'post', 
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
-                find: this.state.search
-            })
-        })
-            .then(res => res.json())
-            .then(value => {
-                this.setState({vagas: value})
-            });
-
-        this.setState({search: ''});
-    }
-
-    render(){
-        return (
-            <div className='search-bar'>
-                <h1>Busque seu futuro</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <input className='search-input' type='search' name='search' value={this.state.search} onChange={this.handleChange}/>
-                    <button className='search-button' name='q'>Search</button>
-                </form>
-                    {
-                        this.state.vagas?
-                        <div className='job-container'>
-                        <div className='visto'><div className='green-box'></div>Você já visitou!</div>
-                            {
-                                
-                                this.state.vagas.map(emp => (
-                                    <JobItem key={emp.id} id={emp.id} vagas={emp.vaga} url={emp.url}/>
-                                ))
-                            }
-                        </div>
-                        : null
-                    }
-            </div>
-        );
-    }
-}
+const SearchBar = ({ vagas, search, handleChange, handleSubmit}) => (
+    <div className='search-bar'>
+        <h1>Busque seu futuro</h1>
+        <form onSubmit={handleSubmit}>
+            <input className='search-input' type='search' name='search' value={search} onChange={handleChange}/>
+            <button className='search-button' name='q'>Search</button>
+        </form>
+    </div>
+)
 
 export default SearchBar;
